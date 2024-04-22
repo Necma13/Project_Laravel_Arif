a6eab459f0979c1116a4a58b723e78e4afce7bd6
Necma13 <145630799+Necma13@users.noreply.github.com>
2024-04-22 11:20:24 +0700

    make database & npm

---
+++ Project/database/migrations/2024_04_22_023956_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim', 15)->unique();
+            $table->string('nama', 35);
+            $table->string('tempat', 35);
+            $table->date('tanggal');
+            $table->string('alamat', 100);
+            $table->string('jenis_kelamin', 100);
+            $table->bigInteger('jurusan');
+            $table->string('agama');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
